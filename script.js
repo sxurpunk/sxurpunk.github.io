ffcec596c9c2c4f227d6725e3c16a1264714c34f
surya <surya.iyer2004@gmail.com>
2025-10-16 01:38:32 -0400

    -

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // YOUR CODE HERE:
             statusDisplay.className = 'statusDisplay.success';
             statusMessage.textContent = 'Success!';
+            console.log('data loaded');
             toggleMethodButtons(true);
             loadButton.disabled = false;
             
