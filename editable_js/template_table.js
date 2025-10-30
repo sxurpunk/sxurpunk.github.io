45a68d9a7a609cbbbc7182f5888a19ce2f2bfd42
surya <surya.iyer2004@gmail.com>
2025-10-30 03:04:24 -0400

    man

--- editable_js/template_table.js
+++ editable_js/template_table.js
@@ -12,7 +12,7 @@ function showTable(data) {
           })
               
           punishmentMoves.forEach(function (move) {
-              innerHtml += `
+              `
             <div className="restaurant-card">
             <h3>${move.command}</h3>
             <p><strong>Hit Level:</strong> ${move.hit_level}</p>
@@ -23,28 +23,14 @@ function showTable(data) {
   `
           })}
               
-      )  
+      ).join("")
   return `
                 <h2 class="view-title">Key Punishment Moves</h2>
                 <p class="view-description">Jin Kazama's core punishment tools.</p>
+                <div class="card-grid">
+                ${punishmentHTML}
+                </div>
             `;
 }
-
-/*const punishmentFilter = jin.filter(function (jin) {
-    return jin.block_frame = '+6';
-})
-
-function showTable(data) {
-    const punishmentMoves = data
-    punishmentFilter.forEach(function (move) {
-       html+= ` <div className="restaurant-card">
-            <h3>${move.command}</h3>
-            <p><strong>Hit Level:</strong> ${move.hit_level}</p>
-            <p><strong>Damage:</strong> ${move.damage}</p>
-            <p><strong>Startup Frames:</strong> ${move.start_up_frame}</p>
-            <p><strong>On-Block Frames</strong> ${move.block_frame}</p>
-        </div>
-        `
-    })*/
 
 export default showTable;
