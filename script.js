cffad951b0945baedc4876322fbf5591c0c14448
surya <surya.iyer2004@gmail.com>
2025-10-30 15:20:45 -0400

    igujajgr

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import showCards from './editable_js/template_cards.js';
-import showCategories from './editable_js/template_category.js';
-import showStats from './editable_js/template_stats.js';
 import showTable from './editable_js/template_table.js';
+/*import showCategories from './editable_js/template_category.js';
+import showStats from './editable_js/template_stats.js';*/
 
 import loadData from './editable_js/load_data.js';
 
