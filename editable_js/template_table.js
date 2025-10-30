3f67f81ec8a23fdcc5d67f52f6912b2d51eb2fae
surya <surya.iyer2004@gmail.com>
2025-10-30 02:49:32 -0400

    i need this

--- editable_js/template_table.js
+++ editable_js/template_table.js
@@ -4,7 +4,26 @@
  * Display data in sortable rows - good for scanning specific information
  */
 function showTable(data) {
-    
+  const punishmentHTML = data
+      .map(
+          (jin) => {
+          const punishmentMoves = jin.filter(function (jin) {
+              return jin.block_frame === '-9';
+          })
+              
+          punishmentMoves.forEach(function (move) {
+              innerHtml += `
+            <div className="restaurant-card">
+            <h3>${move.command}</h3>
+            <p><strong>Hit Level:</strong> ${move.hit_level}</p>
+            <p><strong>Damage:</strong> ${move.damage}</p>
+            <p><strong>Startup Frames:</strong> ${move.start_up_frame}</p>
+            <p><strong>On-Block Frames</strong> ${move.block_frame}</p>
+        </div>
+  `
+          })}
+              
+      )  
   return `
                 <h2 class="view-title">Key Punishment Moves</h2>
                 <p class="view-description">Jin Kazama's core punishment tools.</p>
