fea9895373ccc02362e95e6ebb08f0935766b8c6
surya <surya.iyer2004@gmail.com>
2025-10-29 23:16:58 -0400

    :(((((((((((((((((((

--- editable_js/template_table.js
+++ editable_js/template_table.js
@@ -4,15 +4,14 @@
  * Display data in sortable rows - good for scanning specific information
  */
 
-const punishmentFilter = jin.filter(function (jin) {
+/*const punishmentFilter = jin.filter(function (jin) {
     return jin.block_frame = '+6';
 })
 
 function showTable(data) {
     const punishmentMoves = data
-    punishmentMoves.forEach(function (move) {
-        `
-        <div className="restaurant-card">
+    punishmentFilter.forEach(function (move) {
+       html+= ` <div className="restaurant-card">
             <h3>${move.command}</h3>
             <p><strong>Hit Level:</strong> ${move.hit_level}</p>
             <p><strong>Damage:</strong> ${move.damage}</p>
@@ -20,7 +19,7 @@ function showTable(data) {
             <p><strong>On-Block Frames</strong> ${move.block_frame}</p>
         </div>
         `
-    })
+    })*/
 
     return `
                 <h2 class="view-title">Key Punishment Moves</h2>
